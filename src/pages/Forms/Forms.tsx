1bcdba18de12b7f8621825baf233d96756f62b3a
Danil <danylo.makhov@nure.ua>
2023-03-27 01:09:54 +0300

    fix: change setState to setStateAsync

--- src/pages/Forms/Forms.tsx
+++ src/pages/Forms/Forms.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
 import React, { Component, RefObject, createRef } from 'react';
 import { IFormCard } from '../../types/types';
+import './Forms.css'
+type PartialState<State> = Partial<State> | ((prevState: State) => Partial<State>);
 
-export class Forms extends Component<unknown, { cards: IFormCard[] }> {
+interface ComponentWithSetStateAsync<State> extends React.Component {
+  setStateAsync(state: PartialState<State>): Promise<void>;
+}
+export class Forms extends Component<unknown, { cards: IFormCard[], errors: [] | string[] }> {
   firstName: RefObject<HTMLInputElement>;
   lastName: RefObject<HTMLInputElement>;
   birthday: RefObject<HTMLInputElement>;
@@ -15,7 +20,6 @@ export class Forms extends Component<unknown, { cards: IFormCard[] }> {
   coolWebsite: RefObject<HTMLInputElement>;
   coolFroms: RefObject<HTMLInputElement>;
   whoAreYou: RefObject<HTMLInputElement>;
-  errors: [] | string[];
   constructor(props: IFormCard) {
     super(props);
     this.firstName = createRef();
@@ -30,11 +34,16 @@ export class Forms extends Component<unknown, { cards: IFormCard[] }> {
     this.coolWebsite = createRef();
     this.coolFroms = createRef();
     this.whoAreYou = createRef();
-    this.errors = [];
     this.state = {
+      errors: [],
       cards: [],
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  setStateAsync<State>(this: ComponentWithSetStateAsync<State>, state: PartialState<State>): Promise<void> {
+    return new Promise(resolve => {
+      this.setState(state, resolve);
+    });
   }
   getWhoAreYou() {
     if (this.whoAreYou.current) {
@@ -47,56 +56,61 @@ export class Forms extends Component<unknown, { cards: IFormCard[] }> {
       return checked.id
     }
   }
-  validation() {
+  async setNewError(error: string) {
+    await this.setStateAsync({ cards: this.state.cards, errors: [...this.state.errors, error] })
+  }
+  async validation() {
     if (this.firstName.current?.value === undefined || this.firstName.current?.value.length === 0) {
-      this.errors = [...this.errors, 'firstName']
+      await this.setNewError('firstName')
     }
     if (this.whoAreYou.current) {
       const whoAreYou: HTMLInputElement[] = Array.from(this.whoAreYou.current.querySelectorAll('input[type="radio"]'))
       const checked = whoAreYou.find((radio: HTMLInputElement) => radio.checked)
 
       if (!checked) {
-        this.errors = [...this.errors, 'whoAreYou']
+        await this.setNewError('whoAreYou')
       }
     }
 
     if (this.lastName.current?.value === undefined || this.lastName.current?.value.length === 0) {
-      this.errors = [...this.errors, 'lastName']
+      await this.setNewError('lastName')
     }
 
     if (this.birthday.current?.value === undefined || this.birthday.current?.value.length === 0) {
-      this.errors = [...this.errors, 'birthday']
+      await this.setNewError('birthday')
     }
     if (this.file.current?.files?.[0] === undefined || this.file.current?.files?.[0] === null) {
-      this.errors = [...this.errors, 'files']
+      await this.setNewError('files')
     }
     if (this.country.current?.value === undefined) {
-      this.errors = [...this.errors, 'country']
+      await this.setNewError('country')
     }
     if (this.address.current?.value === undefined || this.address.current?.value.length === 0) {
-      this.errors = [...this.errors, 'address']
+      await this.setNewError('address')
     }
     if (this.city.current?.value === undefined || this.city.current?.value.length === 0) {
-      this.errors = [...this.errors, 'city']
+      await this.setNewError('city')
     }
     if (this.emailNotification.current?.checked === undefined) {
-      this.errors = [...this.errors, 'emailNotification']
+      await this.setNewError('emailNotification')
     }
     if (this.phoneNotification.current?.checked === undefined) {
-      this.errors = [...this.errors, 'phoneNotification']
+      await this.setNewError('phoneNotification')
     }
     if (this.coolWebsite.current?.checked === undefined) {
-      this.errors = [...this.errors, 'coolWebsite']
+      await this.setNewError('coolWebsite')
     }
     if (this.coolFroms.current?.checked === undefined) {
-      this.errors = [...this.errors, 'coolFroms']
+      await this.setNewError('coolFroms')
     }
+    console.log(this.state.errors)
   }
-  handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    await this.setStateAsync({ cards: this.state.cards, errors: [] })
     this.validation()
     const whoAreYou = this.getWhoAreYou()
-    if (this.errors.length === 0 && whoAreYou !== null) {
+    if (this.state.errors.length === 0 && whoAreYou !== null) {
       this.setState({
         cards: [...this.state.cards, {
           firstName: this.firstName.current?.value as string,
@@ -121,16 +135,20 @@ export class Forms extends Component<unknown, { cards: IFormCard[] }> {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className='form-wrapper'>
           <div className="personal-wrapper">
             <h3>Personal information</h3>
             <span>First Name</span>
+            {this.state.errors.find((item) => item === 'firstName') ? <div className='error-text'>Bad data</div> : null}
             <input type="text" ref={this.firstName} />
             <span>Last Name</span>
+            {this.state.errors.find((item) => item === 'lastName') ? <div className='error-text'>Bad data</div> : null}
             <input type="text" ref={this.lastName} />
             <span>Birthday</span>
+            {this.state.errors.find((item) => item === 'birthday') ? <div className='error-text'>Bad data</div> : null}
             <input type="date" ref={this.birthday} />
             <span>Upload profile picture</span>
+            {this.state.errors.find((item) => item === 'files') ? <div className='error-text'>Bad data</div> : null}
             <input type="file" id="" ref={this.file} />
           </div>
           <div className="addres-wrapper">
@@ -142,12 +160,14 @@ export class Forms extends Component<unknown, { cards: IFormCard[] }> {
               <option value="kz">kazakhstan</option>
             </select>
             <span>City</span>
+            {this.state.errors.find((item) => item === 'city') ? <div className='error-text'>Bad data</div> : null}
             <input type="text" ref={this.city} />
             <span>Addres</span>
+            {this.state.errors.find((item) => item === 'address') ? <div className='error-text'>Bad data</div> : null}
             <input type="text" ref={this.address} />
           </div>
           <div className="contact-wrapper">
-            <span>email</span>
+            <span>Email</span>
             <div className="contact-item">
               <input type="checkbox" ref={this.emailNotification} />
               <span>Receive notifications by mail</span>
@@ -171,6 +191,8 @@ export class Forms extends Component<unknown, { cards: IFormCard[] }> {
           </div>
           <div className="radio-wrapper" ref={this.whoAreYou}>
             <span>Who are you</span>
+
+            {this.state.errors.find((item) => item === 'whoAreYou') ? <div className='error-text'>Bad data</div> : null}
             <div className="radio-item">
               <input type="radio" name="radio" id="doomer" className='radio-input' />
               Doomer
